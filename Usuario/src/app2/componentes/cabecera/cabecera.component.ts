8f00389cb63ebde48b0b300937c09d1f7ccb5a3d
Federico Nickler <fedenickler@gmail>
2022-03-09 18:22:46 -0300

    Agrego Usuario

---
+++ Usuario/src/app2/componentes/cabecera/cabecera.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {ApiService} from '../../servicios/api/api.service';
+import {LoginI} from '../../modelos/login.interface';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css'],
+})
+export class CabeceraComponent implements OnInit {
+  loginForm = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(private api:ApiService) {}
+
+  ngOnInit(): void {}
+
+  onLogin(form:LoginI) {
+    console.log(form);
+  }
+}
