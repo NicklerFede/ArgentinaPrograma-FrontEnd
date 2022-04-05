01fdcb63d79a7fb5784bc04a6de6e7f245d420a2
Federico Nickler <fedenickler@gmail>
2022-04-05 17:00:25 -0300

    Union backend con frontend

--- PortafolioFede4/src/app/app-routing.module.ts
+++ PortafolioFede4/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { PortfolioComponent } from './componentes/portfolio/portfolio.component'
 import { UsuarioComponent } from './componentes/usuario/usuario.component';
 
 const routes: Routes = [
-  {path:'', component: PortfolioComponent},
-  {path:'usuario', component:UsuarioComponent}
+  {path:'portfolio', component: PortfolioComponent},
+  {path:'administrador', component:UsuarioComponent},
+  {path:'', redirectTo: 'portfolio', pathMatch:'full'}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PortafolioFede4/src/app/componentes/cabecera/cabecera.component.ts
+++ PortafolioFede4/src/app/componentes/cabecera/cabecera.component.ts
@@ -1,18 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.css'],
 })
-export class CabeceraComponent implements OnInit { 
+export class CabeceraComponent implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.form=this.formBuilder.group({
-    username:['',[Validators.required, Validators.minLength(4),Validators.maxLength(20)]],
-    password:['',[Validators.required,Validators.minLength(4),Validators.maxLength(20)]],
-    })
+  constructor(
+    private formBuilder: FormBuilder,
+    private autenticacionService: AutenticacionService,
+    private ruta: Router
+  ) {
+    this.form = this.formBuilder.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+    });
   }
+
   ngOnInit(): void {}
+
+  get username() {
+    return this.form.get('username');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  onEnviar(event: Event) {
+    event.preventDefault();
+    this.autenticacionService
+      .IniciarSesion(this.form.value)
+      .subscribe((data) => {
+        console.log('DATA' + JSON.stringify(data));
+        this.ruta.navigate;
+      });
+      this.ruta.navigate(['/administrador']);
+  }
 }
